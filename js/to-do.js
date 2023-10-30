815e7043ba3ce84211933ac5599b855441ac71bc
Fabrício Lustosa <lustosacosta7@gmail.com>
2023-10-30 00:02:58 -0300

    mais um pouco de js

--- js/to-do.js
+++ js/to-do.js
@@ -1,12 +1,21 @@
 function addTask() {
   const inputElement = document.querySelector("#input").value;
-  const tasksDiv = document.getElementById("tasks-list");
   const textAlert = document.getElementById("text-alert");
-  const taskList = document.querySelector("task-list");
+  const tasksList = document.querySelector("#tasks-list");
+
   if (inputElement === "") {
     // Criar um novo elemento para a tarefa
     textAlert.innerHTML = "Digite um valor válido";
+    textAlert.style.color = "#fb3640";
+    textAlert.style.backgroundColor = "white";
   } else {
-    taskList.textContent = inputElement;
+    const newTask = document.createElement("div");
+    newTask.textContent = inputElement.value;
+    tasksList.appendChild(newTask);
+
+    textAlert.innerHTML = "Aqui abaixo vai aparecer suas tarefas...";
+    textAlert.style.color = "white";
+    textAlert.style.backgroundColor = "#fb3640";
+    inputElement.value = "";
   }
 }
